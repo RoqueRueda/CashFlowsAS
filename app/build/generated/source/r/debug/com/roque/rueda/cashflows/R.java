55b79b088e0608973d03ec07484c9da97ebc59d7
Roque Rueda <skaroque@gmail.com>
2015-03-31 08:41:25 -0600

    Alpha Version
    Need to add images.

--- app/build/generated/source/r/debug/com/roque/rueda/cashflows/R.java
+++ app/build/generated/source/r/debug/com/roque/rueda/cashflows/R.java
@@ -221,6 +221,7 @@ containing a value of this type.
     public static final class menu {
         public static final int add_amount=0x7f090000;
         public static final int main=0x7f090001;
+        public static final int movements_menu=0x7f090002;
     }
     public static final class string {
         public static final int accept=0x7f070000;
